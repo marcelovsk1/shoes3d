94ec1c0c870686c7ae758ae62dd278f4d3f6ba30
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-25 16:04:49 -0300

    Custom Scene View

---
+++ nike_3d/View/CustomSceneView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSceneView.swift
+//  nike_3d
+//
+//  Created by Marcelo Amaral Alves on 2024-04-25.
+//
+
+import SwiftUI
+
+struct CustomSceneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSceneView()
+}
