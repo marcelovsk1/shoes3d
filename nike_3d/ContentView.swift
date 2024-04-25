5ce6f6012c35071afb3a880289233d944e2f9b25
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-25 15:58:02 -0300

    Initial Commit

---
+++ nike_3d/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nike_3d
+//
+//  Created by Marcelo Amaral Alves on 2024-04-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nike_3d/nike_3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  nike_3dApp.swift
+//  nike_3d
+//
+//  Created by Marcelo Amaral Alves on 2024-04-25.
+//
+
+import SwiftUI
+
+@main
+struct nike_3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
